149ae2a50386576b3e73e277cd4c7d3e3268459d
yir6614 <Yamil.Irizarry@ramajudicial.pr>
2019-06-28 14:41:28 -0800

    Created Task model and serializer

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,20 @@
+from datetime import datetime
+
 from django.db import models
+
+VERY_LOW = 1
+LOW = 2
+MEDIUM = 3
+HIGH = 4
+TOP = 5
+
+PRIORITY_CHOICES = (
+    (VERY_LOW, "Very Low Priority"),
+    (LOW, "Low Priority"),
+    (MEDIUM, "Medium Priority"),
+    (HIGH, "High Priority"),
+    (TOP, "Top Priority"),
+)
 
 
 class TodoList(models.Model):
@@ -6,6 +22,16 @@ class TodoList(models.Model):
 
 
 class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField('date created', default=datetime.now())
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=MEDIUM)
+    done = models.BooleanField(default=False)
+    list = "list"
+
+    class Meta:
+        ordering = ('priority',)
+
     pass
 
 

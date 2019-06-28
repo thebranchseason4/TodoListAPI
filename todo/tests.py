41880769a58079b1f8978cf6d0dce6d361f42535
crm9770 <Cristian.Rosado@ramajudicial.pr>
2019-06-28 16:06:17 -0400

    testing travis setup

--- todo/tests.py
+++ todo/tests.py
@@ -1,1 +1,9 @@
 # Create your tests here.
+from rest_framework.test import APITestCase
+
+class PracticesTests(APITestCase):
+    def passTravisTest(self):
+        self.assertEqual(1,1)
+
+    def failedTravisTest(self):
+        self.assertEqual(1,2)

7836c3334efbcf52b45fda3d290a624edda8aa7d
crm9770 <Cristian.Rosado@ramajudicial.pr>
2019-06-28 16:53:17 -0400

    final travis test

--- todo/tests.py
+++ todo/tests.py
@@ -4,6 +4,3 @@ from rest_framework.test import APITestCase
 class PracticesTestCase(APITestCase):
     def test_pass(self):
         self.assertEqual(1, 1)
-
-    def test_fail(self):
-        self.assertEqual(1, 2)

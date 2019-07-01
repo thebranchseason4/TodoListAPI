56d261069145de488291cde8e72072d9ca15eb9a
jrr0360 <jose.rivera232@upr.edu>
2019-07-01 10:33:01 -0400

    Finished Todo in TodoList Serializer

--- todo/tests.py
+++ todo/tests.py
@@ -1,11 +1,9 @@
 
-from django.core import serializers
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from django.test import TestCase
 
-from todo.serializers import TaskSerializer
 from .models import *
 from datetime import datetime
 
